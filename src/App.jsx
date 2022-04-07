2014ec350d1647d2972e25a594e891e49ab0629c
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-04-07 22:06:48 +0200

    initialized Register

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import './App.css';
-import Home from './Pages/Home/Home';
-import TopBar from './TopBar/TopBar';
+import "./App.css";
+import Home from "./Pages/Home/Home";
+import Register from "./Register/Register";
+import TopBar from "./TopBar/TopBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    /*  <>
     <TopBar/>
     <Home/>
-    
-    </>
-    
+    <Register/>
+    </> */
+    <Router>
+      <TopBar />
+         <Register />
+    </Router>
   );
-}
+};
 export default App;
