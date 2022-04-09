1c320588f3e3677a8b5d9ff724de13c1729aec20
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-04-09 22:00:24 +0200

    changes to links

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-Register
+Register;
 import "./App.css";
 import Home from "./Pages/Home/Home";
 import Register from "./Register/Register";
 import TopBar from "./TopBar/TopBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
- master
+master;
 
 const App = () => {
   return (
-    /*  <>
-    <TopBar/>
-    <Home/>
-
-    <Register/>
-    </> */
-    <Router>
-      <TopBar />
-         <Register />
-    </Router>
-
-    </>
-
+    <div>
+      <Router>
+        <TopBar />
+        <Register />
+      </Router>
+    </div>
   );
 };
 export default App;
