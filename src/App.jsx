d132fb39f94ca8d5946b8b2cc0ee8b2f3679b189
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-04-09 22:06:34 +0200

    Topbar corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-Register;
+
 import "./App.css";
-import Home from "./Pages/Home/Home";
+
 import Register from "./Register/Register";
-import TopBar from "./TopBar/TopBar";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-master;
+
 
 const App = () => {
   return (
     <div>
       <Router>
-        <TopBar />
+      <TopBar/>
         <Register />
       </Router>
     </div>
