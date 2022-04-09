9aa3b0a49cba2d710253afdac83549d36ea46c4c
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-04-09 20:26:33 +0200

    fixed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-Register;
 import "./App.css";
-import Home from "./Pages/Home/Home";
+import Home from "./Components/Pages/Home/Home";
 import Register from "./Register/Register";
-import TopBar from "./TopBar/TopBar";
+import TopBar from "./Components/TopBar/TopBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-master;
 
 const App = () => {
   return (
