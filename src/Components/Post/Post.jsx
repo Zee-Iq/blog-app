fb84d8a7831b310b3228738d5914923fd3b93b7a
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-04-10 17:25:18 +0200

    Added postDescription styling, added flex wrap to posts

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -14,12 +14,14 @@ const Post = () => {
           <span className="postCat">Lifestyle</span>
         </div>
         <span className="postTitle">
-          Lorem, ipsum dolor sit amet consectetur 
+          Lorem, ipsum dolor sit amet consectetur
         </span>
-        <hr />
+        
         <span className="postDate"> 1 hour ago</span>
       </div>
-      <p className="postDescription">Lorem ipsum, dolor sit amet consectetur adipisicing elit. At quia consectetur, atque expedita quae totam vero ratione facere eius autem illo labore temporibus sed, enim repellat saepe, corporis sunt possimus?</p>
+      <p className="postDescription">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum voluptates voluptate quas suscipit facere ducimus exercitationem laborum veritatis molestiae distinctio soluta, id at rerum pariatur temporibus enim inventore doloremque sed numquam repellat velit fugiat expedita minus et? Quasi fugit dignissimos perspiciatis, quod blanditiis repellat omnis quibusdam odit nostrum consectetur! Accusamus molestiae consequatur vero! Error quos optio, explicabo nihil pariatur enim rerum ut animi ea voluptatum quis, excepturi possimus. Sequi in rem eligendi culpa tempora asperiores id, excepturi vitae molestias, dolore nemo consectetur saepe quibusdam? Veniam earum, totam aut perferendis, minima, ipsam maxime officiis libero architecto neque sequi eveniet nisi laboriosam! Voluptatum fugiat quibusdam soluta eius in. Quia, quod inventore cumque totam facilis sequi. In quia officiis magnam nemo impedit dignissimos labore, porro maiores hic quo? Sed neque ipsa vitae modi voluptatem magnam eos inventore fugit odio. Magni dicta, at maxime nostrum eum vitae atque nihil accusamus eveniet, quia numquam delectus sit, commodi inventore provident adipisci eligendi. Impedit cumque tempore dignissimos delectus. Mollitia earum in quasi harum vel iusto fuga labore numquam facilis, distinctio expedita, repellat, odio iste et sapiente dolorum sequi amet aliquid. Quia iste facere ullam ducimus doloribus, molestiae illo quidem, eum, officia totam dicta eius hic in repellat?
+      </p>
     </div>
   );
 };
