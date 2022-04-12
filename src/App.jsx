f3322e44dd99d4d12034163f1a9c72cbd6f9fb34
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-04-12 11:12:42 +0200

    fixed error in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import './App.css';
+import "./App.css";
 import Write from "./Components/Write/Write";
-import TopBar from './Components/TopBar/TopBar';
-
 import TopBar from "./Components/TopBar/TopBar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-
 
 const App = () => {
   return (
     <>
-    <TopBar/>
-    <Write />    
+      <TopBar />
+      <Write />
     </>
   );
 };
