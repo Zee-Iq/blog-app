cda18512bc788042454c2c5691fb4a3798d3a66d
ChristopherA81 <cdavid81@gmail.com>
2022-04-13 11:17:24 +0200

    created const post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -16,6 +16,29 @@ router.post("/", async (req, res) => {
     }
 });
 
-//Log in
+router.put ("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username) { 
+        try {
+            const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
+                $set:req.body
+            })
+            
+        } catch (err) {
+
+        }
+    } else {
+        res.status(401).json("You can update only your post")
+    }
+
+    } catch (err) {
+        res.status(500).json(err)
+
+    }
+
+});
+
+
 
 module.exports = router
