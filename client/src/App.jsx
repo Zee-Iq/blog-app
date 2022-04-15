aac163e33ebcd56fd492923d2f68c07127629a5e
Igor <machold.igor@gmail.com>
2022-04-15 13:02:00 +0200

    second try

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import TopBar from "./Components/TopBar/TopBar";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Homepage from "./Components/Homepage/Homepage";
 import Register from "./Components/Register/Register";
 import Login from "./Components/Login/Login";
@@ -8,8 +8,7 @@ import Write from "./Components/Write/Write";
 import UserProfile from "./Components/UserProfile/UserProfile";
 import Single from "./Components/Single/Single";
 import EmailConfirm from "./Components/EmailConfirm/EmailConfirm";
-
-
+import ForgotPass from "./Components/ForgotPassword/ForgotPass";
 
 const user = false;
 /* test */
@@ -29,10 +28,15 @@ const App = () => {
           path="/userprofile"
           element={user ? <UserProfile /> : <Register />}
         ></Route>
-         <Route path="/post" element={<Single />}> </Route> 
-         <Route path="/emailconfirm/:token" exact element={<EmailConfirm />}></Route>
-         {/* <Route path='/forgotpass' exact component={}></Route> */}
-         
+        <Route path="/post" element={<Single />}>
+          {" "}
+        </Route>
+        <Route
+          path="/emailconfirm/:token"
+          exact
+          element={<EmailConfirm />}
+        ></Route>
+        <Route path="/forgotpass" element={<ForgotPass/>} exact></Route>
       </Routes>
     </Router>
   );
--- client/src/Components/ForgotPassword/ForgotPass.jsx
+++ client/src/Components/ForgotPassword/ForgotPass.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 
 
+
 export default function ForgotPass() {
-  // const [email, setEmail] = useState;
+  const [email, setEmail] = useState;
 
   return (
     <div className="password-container">
-      {/* <p>Please type your email to help recover your password</p>
+      <p>Please type your email to help recover your password</p>
       <input
         placeholder="Type your email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
-      <button>Send</button> */}
+      <button>Send</button>
       <h1>TEST !!!!!!!!!!!!!!!!!!</h1>
     </div>
   );
