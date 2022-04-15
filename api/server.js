a5e07c98443a222da51289cbc9406899584b5a7c
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-04-15 20:01:39 +0200

    connecting express with mongoDB

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+
+app.use("/", (req,res) =>{
+    console.log("Hello from /")
+})
 
 app.listen("5500", () => {
-console.log("server is running at port 5500");
-})
+  console.log("server is running at port 5500");
+});
