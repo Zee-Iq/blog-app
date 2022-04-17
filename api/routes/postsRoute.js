6c29bf2e82da09f1eb3b441271d38f0f6b2eb0c4
Zeeshan <zeeshan.iqbal@hotmail.de>
2022-04-17 16:41:37 +0200

    Finished and Tested API

--- api/routes/postsRoute.js
+++ api/routes/postsRoute.js
@@ -1,49 +1,98 @@
 const router = require("express").Router();
 const User = require("../models/UserModel");
 const Post = require("../models/PostModel");
+const { json } = require("express");
 
 //CREATE POST
 router.post("/", async (req, res) => {
   const newPost = new Post(req.body);
   try {
-    const savedPost = newPost.save();
+    const savedPost = await newPost.save();
     res.status(200).json(savedPost);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-//UPDATE USER
-router.delete("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id) {
-    try {
-      const user = await User.findById(req.params.id);
+//UPDATE POST
+// Logic --> 1.Find the Post --> 2. Check if Username of the post matches the username of the author --> If they match... the post  can updated
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
       try {
-        await Post.deleteMany({ username: user.username });
-        await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User has been deleted successfully.");
-      } catch (error) {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
+      } catch (err) {
         res.status(500).json(err);
       }
-    } catch (err) {
-      res.status(404).json("User not found!");
+    } else {
+      res.status(401).json("Unauthorized action, you can update only your post!");
     }
-  } else {
-    res.status(401).json("You can only delete your own account!");
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
 //DELETE POST
 
-//GET POST
-router.get("/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
-  } catch (error) {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        res.status(200).json("Post has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can delete only your post!");
+    }
+  } catch (err) {
     res.status(500).json(err);
   }
 });
+
+//GET POST
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL POSTS
+router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await Post.find();
+    }
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,26 +2,38 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const authRoute = require("./routes/authRoute")
-const userRoute = require("./routes/usersRoute")
-
+const authRoute = require("./routes/authRoute");
+const userRoute = require("./routes/usersRoute");
+const postRoute = require("./routes/postsRoute");
+const categoryRoute = require("./routes/categoriesRoute");
+const multer = require("multer");
 
 dotenv.config();
-app.use(express.json())
+app.use(express.json());
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "hello.jpg");
+  },
+});
 
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
-mongoose.connect(process.env.MONGO_URL,)
-.then(console.log("Connected to MongoDB"))
-.catch((err) => console.log(err))
-
-app.get("/greeting", (req,res) => {
-  res.json({greeting:"hello from /greeing"})
-})
-
-app.use("/api/auth", authRoute)
-app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5500", () => {
   console.log("server is running at port 5500");
